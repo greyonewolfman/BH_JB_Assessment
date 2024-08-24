5e2058142e20f39e4015483fc660cf3890decec9
g1projectsaz1 <>
2024-08-24 16:42:26 +0200

    main

--- aspnet-core-dotnet-core/Terraform/webapp_with_autoscale_template.tf
+++ aspnet-core-dotnet-core/Terraform/webapp_with_autoscale_template.tf
@@ -29,14 +29,6 @@ resource "azurerm_linux_web_app" "WebApp" {
   site_config {
     always_on                               = true
   }
-
-  app_settings = {
-    "SCM_DO_BUILD_DURING_DEPLOYMENT"        = "true"
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE"   = "false"
-    "docker_registry_url"                   = var.DOCKER_REGISTRY_SERVER_URL_VAR
-    "docker_registry_username"              = var.DOCKER_REGISTRY_SERVER_USERNAME_VAR
-    "docker_registry_password"              = var.DOCKER_REGISTRY_SERVER_PASSWORD_VAR
-  }
 }
 
 output "webapp_name" {

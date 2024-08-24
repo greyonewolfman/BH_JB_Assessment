64b0257484dcb26021736472bc0a5f195936d59c
g1projectsaz1 <>
2024-08-24 12:05:57 +0200

    main

--- aspnet-core-dotnet-core/Terraform/webapp_with_autoscale_template.tf
+++ aspnet-core-dotnet-core/Terraform/webapp_with_autoscale_template.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azuredevops = {
-      source                             = "microsoft/azuredevops"
-      version                            = "1.2.0"
+      source                              = "microsoft/azuredevops"
+      version                             = "1.2.0"
     }
   }
 }
@@ -13,29 +13,29 @@ terraform {
 }
 
 resource "azurerm_service_plan" "ServicesPlan" {
-  name                                    = var.WEBAPP_PLAN_NAME
-  resource_group_name                     = var.RG_NAME
-  location                                = var.LOCATION
-  os_type                                 = var.OS_TYPE
-  sku_name                                = var.SKU_NAME
+  name                                      = var.WEBAPP_PLAN_NAME
+  resource_group_name                       = var.RG_NAME
+  location                                  = var.LOCATION
+  os_type                                   = var.OS_TYPE
+  sku_name                                  = var.SKU_NAME
 }
 
 resource "azurerm_linux_web_app" "WebApp" {
-  name                                    = var.WEB_APP_NAME
-  resource_group_name                     = var.RG_NAME
-  location                                = azurerm_service_plan.ServicesPlan.location
-  service_plan_id                         = azurerm_service_plan.ServicesPlan.id
+  name                                      = var.WEB_APP_NAME
+  resource_group_name                       = var.RG_NAME
+  location                                  = azurerm_service_plan.ServicesPlan.location
+  service_plan_id                           = azurerm_service_plan.ServicesPlan.id
 
   site_config {
-    always_on                             = true
+    always_on                               = true
   }
 
   app_settings = {
-    "SCM_DO_BUILD_DURING_DEPLOYMENT"      = "true"
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-    "docker_registry_url"                 = var.DOCKER_REGISTRY_SERVER_URL
-    "docker_registry_username"            = var.DOCKER_REGISTRY_SERVER_USERNAME
-    "docker_registry_password"            = var.DOCKER_REGISTRY_SERVER_PASSWORD
+    "SCM_DO_BUILD_DURING_DEPLOYMENT"        = "true"
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE"   = "false"
+    "docker_registry_url"                   = var.DOCKER_REGISTRY_SERVER_URL
+    "docker_registry_username"              = var.DOCKER_REGISTRY_SERVER_USERNAME
+    "docker_registry_password"              = var.DOCKER_REGISTRY_SERVER_PASSWORD
   }
 }
 
@@ -45,54 +45,54 @@ output "webapp_name" {
 
 ### To Create the Azure Auto Scale based on the CPU Percentage ################
 resource "azurerm_monitor_autoscale_setting" "autoscale" {
-  name                                    = var.APP_AUTOSCALE
-  resource_group_name                     = azurerm_service_plan.ServicesPlan.resource_group_name
-  location                                = azurerm_service_plan.ServicesPlan.location
-  target_resource_id                      = azurerm_service_plan.ServicesPlan.id
+  name                                      = var.APP_AUTOSCALE
+  resource_group_name                       = azurerm_service_plan.ServicesPlan.resource_group_name
+  location                                  = azurerm_service_plan.ServicesPlan.location
+  target_resource_id                        = azurerm_service_plan.ServicesPlan.id
 
   profile {
     name = "defaultProfile"
     capacity {
-      default = 1
-      minimum = 1
-      maximum = 2
+      default                               = 1
+      minimum                               = 1
+      maximum                               = 2
     }
     rule {
       metric_trigger {
-        metric_name        = "CpuPercentage"
-        metric_resource_id = azurerm_service_plan.ServicesPlan.id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "GreaterThan"
-        threshold          = 5
+        metric_name                         = "CpuPercentage"
+        metric_resource_id                  = azurerm_service_plan.ServicesPlan.id
+        time_grain                          = "PT1M"
+        statistic                           = "Average"
+        time_window                         = "PT5M"
+        time_aggregation                    = "Average"
+        operator                            = "GreaterThan"
+        threshold                           = 5
       }
 
       scale_action {
-        direction = "Increase"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
+        direction                           = "Increase"
+        type                                = "ChangeCount"
+        value                               = "1"
+        cooldown                            = "PT1M"
       }
     }
 
     rule {
       metric_trigger {
-        metric_name        = "CpuPercentage"
-        metric_resource_id = azurerm_service_plan.ServicesPlan.id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "LessThan"
-        threshold          = 80
+        metric_name                         = "CpuPercentage"
+        metric_resource_id                  = azurerm_service_plan.ServicesPlan.id
+        time_grain                          = "PT1M"
+        statistic                           = "Average"
+        time_window                         = "PT5M"
+        time_aggregation                    = "Average"
+        operator                            = "LessThan"
+        threshold                           = 80
       }
       scale_action {
-        direction = "Decrease"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
+        direction                           = "Decrease"
+        type                                = "ChangeCount"
+        value                               = "1"
+        cooldown                            = "PT1M"
       }
     }
   }
